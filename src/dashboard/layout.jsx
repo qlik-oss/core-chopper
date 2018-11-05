d7166bb8be9c0360974d2e6d31b11fe636dce1b0
Andrée Hansson <peolanha@gmail.com>
2018-11-06 00:54:20 +0100

    Fix last class

--- src/dashboard/layout.jsx
+++ src/dashboard/layout.jsx
@@ -7,26 +7,14 @@ import CaloryChart from './calory-chart';
 
 import './layout.css';
 
-export default class Dashboard extends React.Component {
-  constructor({ player, socket }) {
-    super();
-    this.state = { player, socket };
-  }
-
-  componentWillReceiveProps({ player }) {
-    this.setState({ player });
-  }
-
-  render() {
-    const { player, socket } = this.state;
-    return (
-      <div className="dashboard">
-        <HighScore player={player} />
-        <Scan player={player} socket={socket} />
-        <HighScore player={player} distinct />
-        <CaloryChart player={player} />
-        <PowerChart player={player} />
-      </div>
-    );
-  }
+export default function ({ player, socket }) {
+  return (
+    <div className="dashboard">
+      <HighScore player={player} />
+      <Scan player={player} socket={socket} />
+      <HighScore player={player} distinct />
+      <CaloryChart player={player} />
+      <PowerChart player={player} />
+    </div>
+  );
 }
